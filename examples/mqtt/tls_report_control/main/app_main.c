3acd6a680c3169a26c3ca6f6191f2beeb9a4c291
Rooney Jang <pimco01@naver.com>
2024-03-30 15:59:35 +0900

    TLS MQTT report & control example upload
    Signed-off-by: Rooney Jang <pimco01@naver.com>

---
+++ examples/mqtt/tls_report_control/main/app_main.c
@@ -0,0 +1,293 @@
+/* tls-mutual-auth example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_netif_ppp.h"
+#include "esp_modem_api.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "protocol_examples_common.h"
+#include <freertos/task.h>
+#include "DHT.h"
+
+#define GPIO_OUTPUT_WAKEUP         (gpio_num_t)GPIO_NUM_19
+#define GPIO_OUTPUT_WAKEUP_PIN_SEL (1ULL<<GPIO_OUTPUT_WAKEUP)
+#define GPIO_OUTPUT_RESET          (gpio_num_t)GPIO_NUM_18
+#define GPIO_OUTPUT_RESET_PIN_SEL  (1ULL<<GPIO_OUTPUT_RESET)
+#define GPIO_OUTPUT_PWRKEY         (gpio_num_t)GPIO_NUM_5
+#define GPIO_OUTPUT_PWRKEY_PIN_SEL (1ULL<<GPIO_OUTPUT_PWRKEY)
+#define GPIO_OUTPUT_EXTANT         (gpio_num_t)GPIO_NUM_4
+#define GPIO_OUTPUT_EXTANT_PIN_SEL (1ULL<<GPIO_OUTPUT_EXTANT)
+
+
+#define GPIO_OUTPUT_RED				(gpio_num_t)GPIO_NUM_12
+#define GPIO_OUTPUT_RED_PIN_SEL		(1ULL<<GPIO_OUTPUT_RED)
+#define GPIO_OUTPUT_GREEN			(gpio_num_t)GPIO_NUM_14
+#define GPIO_OUTPUT_GREEN_PIN_SEL	(1ULL<<GPIO_OUTPUT_GREEN)
+#define GPIO_OUTPUT_BLUE			(gpio_num_t)GPIO_NUM_15
+#define GPIO_OUTPUT_BLUE_PIN_SEL	(1ULL<<GPIO_OUTPUT_BLUE)
+
+#define EXAMPLE_FLOW_CONTROL ESP_MODEM_FLOW_CONTROL_NONE
+#define MODEM_PPP_APN "simplio.apn"
+static const char *TAG = "pppos_example";
+
+static EventGroupHandle_t event_group = NULL;
+static const int CONNECT_BIT = BIT0;
+static const int GOT_DATA_BIT = BIT2;
+static const int USB_DISCONNECTED_BIT = BIT3; // Used only with USB DTE but we define it unconditionally, to avoid too many #ifdefs in the code
+
+esp_err_t esp_modem_get_time(esp_modem_dce_t *dce_wrap, char *p_time);
+
+static const char *TAG2 = "DHT";
+float temp, humi;
+
+static void config_gpio(void)
+{
+	gpio_config_t io_conf = {};                     //zero-initialize the config structure.
+
+	io_conf.intr_type = GPIO_INTR_DISABLE;          //disable interrupt
+	io_conf.mode = GPIO_MODE_OUTPUT;                //set as output mode
+	io_conf.pin_bit_mask = (GPIO_OUTPUT_WAKEUP_PIN_SEL | GPIO_OUTPUT_RESET_PIN_SEL | 
+			GPIO_OUTPUT_PWRKEY_PIN_SEL | GPIO_OUTPUT_EXTANT_PIN_SEL | GPIO_OUTPUT_RED_PIN_SEL |
+			GPIO_OUTPUT_GREEN_PIN_SEL |	GPIO_OUTPUT_BLUE_PIN_SEL );
+	io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;   //disable pull-down mode
+	io_conf.pull_up_en = GPIO_PULLUP_DISABLE;       //disable pull-up mode
+
+	gpio_config(&io_conf);                          //configure GPIO with the given settings
+}
+
+static void gpio_modem(void)
+{
+	/* Power on the modem */
+	ESP_LOGI(TAG, "Power on the modem");
+	gpio_set_level(GPIO_OUTPUT_WAKEUP, 1);
+	gpio_set_level(GPIO_OUTPUT_RESET, 0);
+	gpio_set_level(GPIO_OUTPUT_PWRKEY, 1);
+	gpio_set_level(GPIO_OUTPUT_EXTANT, 1);
+
+	vTaskDelay(pdMS_TO_TICKS(100));
+	gpio_set_level(GPIO_OUTPUT_RESET, 1);
+
+	vTaskDelay(pdMS_TO_TICKS(2000));
+}
+
+static void gpio_led(void)
+{
+	/* Power on the modem */
+	ESP_LOGI(TAG, "led all off");
+	gpio_set_level(GPIO_OUTPUT_RED, 0);
+	gpio_set_level(GPIO_OUTPUT_GREEN, 0);
+	gpio_set_level(GPIO_OUTPUT_BLUE, 0);
+
+	vTaskDelay(pdMS_TO_TICKS(100));
+}
+
+void gpio_color(int value)
+{
+	gpio_led();
+	switch(value){
+		case 4:
+		{
+			gpio_set_level(GPIO_OUTPUT_RED, 1);
+		}
+		break;
+		case 2:
+		{
+			gpio_set_level(GPIO_OUTPUT_GREEN, 1);
+		}
+		break;
+		case 1:
+		{
+			gpio_set_level(GPIO_OUTPUT_BLUE, 1);
+		}
+		break;
+		case 0:
+		{
+		}
+		break;
+	}
+}
+
+static void DHT_task(void *pvParameter)
+{
+    setDHTgpio(GPIO_NUM_25);
+    ESP_LOGI(TAG2, "Starting DHT Task\n\n");
+
+    while (1) 
+    {
+        //ESP_LOGI(TAG2, "=== Reading DHT ===\n");
+        int ret = readDHT();
+
+        errorHandler(ret);
+
+		humi = getHumidity();
+		temp = getTemperature();
+
+        //ESP_LOGI(TAG2, "Hum: %.1f Tmp: %.1f\n", humi, temp);
+
+        // -- wait at least 2 sec before reading again ------------
+        // The interval of whole process must be beyond 2 seconds !!
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }   
+}
+
+void DHT22Task(void){
+	xTaskCreate(&DHT_task, "DHT_task", 2048, NULL, 2, NULL);
+}
+
+
+static void on_ppp_changed(void *arg, esp_event_base_t event_base,
+                           int32_t event_id, void *event_data)
+{
+    ESP_LOGI(TAG, "PPP state changed event %" PRIu32, event_id);
+    if (event_id == NETIF_PPP_ERRORUSER) {
+        /* User interrupted event from esp-netif */
+        esp_netif_t **p_netif = event_data;
+        ESP_LOGI(TAG, "User interrupted event from netif:%p", *p_netif);
+    }
+}
+
+static void on_ip_event(void *arg, esp_event_base_t event_base,
+                        int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "IP event! %" PRIu32, event_id);
+    if (event_id == IP_EVENT_PPP_GOT_IP) {
+        esp_netif_dns_info_t dns_info;
+
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        esp_netif_t *netif = event->esp_netif;
+
+        ESP_LOGI(TAG, "Modem Connect to PPP Server");
+        ESP_LOGI(TAG, "~~~~~~~~~~~~~~");
+        ESP_LOGI(TAG, "IP          : " IPSTR, IP2STR(&event->ip_info.ip));
+        ESP_LOGI(TAG, "Netmask     : " IPSTR, IP2STR(&event->ip_info.netmask));
+        ESP_LOGI(TAG, "Gateway     : " IPSTR, IP2STR(&event->ip_info.gw));
+        esp_netif_get_dns_info(netif, 0, &dns_info);
+        ESP_LOGI(TAG, "Name Server1: " IPSTR, IP2STR(&dns_info.ip.u_addr.ip4));
+        esp_netif_get_dns_info(netif, 1, &dns_info);
+        ESP_LOGI(TAG, "Name Server2: " IPSTR, IP2STR(&dns_info.ip.u_addr.ip4));
+        ESP_LOGI(TAG, "~~~~~~~~~~~~~~");
+        xEventGroupSetBits(event_group, CONNECT_BIT);
+
+        ESP_LOGI(TAG, "GOT ip event!!!");
+    } else if (event_id == IP_EVENT_PPP_LOST_IP) {
+        ESP_LOGI(TAG, "Modem Disconnect from PPP Server");
+    } else if (event_id == IP_EVENT_GOT_IP6) {
+        ESP_LOGI(TAG, "GOT IPv6 event!");
+
+        ip_event_got_ip6_t *event = (ip_event_got_ip6_t *)event_data;
+        ESP_LOGI(TAG, "Got IPv6 address " IPV6STR, IPV62STR(event->ip6_info.ip));
+    }
+}
+
+int aws_iot_demo_main( int argc, char ** argv );
+
+/*
+ * Prototypes for the demos that can be started from this project.  Note the
+ * MQTT demo is not actually started until the network is already.
+ */
+
+void app_main()
+{
+    // Initialize GPIO
+	config_gpio();
+	gpio_modem();
+	gpio_led();
+    
+    /* Init and register system/core components */
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, ESP_EVENT_ANY_ID, &on_ip_event, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(NETIF_PPP_STATUS, ESP_EVENT_ANY_ID, &on_ppp_changed, NULL));
+
+    /* Configure the PPP netif */
+    esp_modem_dce_config_t dce_config = ESP_MODEM_DCE_DEFAULT_CONFIG(MODEM_PPP_APN);
+    esp_netif_config_t netif_ppp_config = ESP_NETIF_DEFAULT_PPP();
+    esp_netif_t *esp_netif = esp_netif_new(&netif_ppp_config);
+    assert(esp_netif);
+
+    event_group = xEventGroupCreate();
+
+    /* Configure the DTE */
+    esp_modem_dte_config_t dte_config = ESP_MODEM_DTE_DEFAULT_CONFIG();
+    /* setup UART specific configuration based on kconfig options */
+    dte_config.uart_config.tx_io_num = 17;
+    dte_config.uart_config.rx_io_num = 16;
+    dte_config.uart_config.rts_io_num = GPIO_NUM_NC;
+    dte_config.uart_config.cts_io_num = GPIO_NUM_NC;
+    dte_config.uart_config.flow_control = ESP_MODEM_FLOW_CONTROL_NONE;
+    dte_config.uart_config.rx_buffer_size = 4096;
+    dte_config.uart_config.tx_buffer_size = 512;
+    dte_config.uart_config.event_queue_size = 30;
+    dte_config.task_stack_size = 4096;
+    dte_config.task_priority = 5;
+    dte_config.dte_buffer_size = 512;
+
+    ESP_LOGI(TAG, "Initializing esp_modem for a generic module...");
+    esp_modem_dce_t *dce = esp_modem_new(&dte_config, &dce_config, esp_netif);
+    assert(dce);
+    if (dte_config.uart_config.flow_control == ESP_MODEM_FLOW_CONTROL_HW) {
+        esp_err_t err = esp_modem_set_flow_control(dce, 2, 2);  //2/2 means HW Flow Control.
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to set the set_flow_control mode");
+            return;
+        }
+        ESP_LOGI(TAG, "HW set_flow_control OK");
+    }
+
+    xEventGroupClearBits(event_group, CONNECT_BIT | GOT_DATA_BIT | USB_DISCONNECTED_BIT);
+
+    esp_err_t err = esp_modem_set_mode(dce, ESP_MODEM_MODE_DATA);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "esp_modem_set_mode(ESP_MODEM_MODE_DATA) failed with %d", err);
+        return;
+    }
+    /* Wait for IP address */
+    ESP_LOGI(TAG, "Waiting for IP address");
+    xEventGroupWaitBits(event_group, CONNECT_BIT | USB_DISCONNECTED_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+        
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %"PRIu32" bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    
+    /* Initialize NVS partition */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        /* NVS partition was truncated
+         * and needs to be erased */
+        ESP_ERROR_CHECK(nvs_flash_erase());
+
+        /* Retry nvs_flash_init */
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
+
+	esp_rom_gpio_pad_select_gpio(GPIO_NUM_25);
+
+//	DHT22Task();
+
+ #if 1   
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    aws_iot_demo_main(0,NULL);
+#endif
+}
